a51c5d76b0738406a51d2033397c5480e3e58752
AndreaFoca <focarelli.1927386@studenti.uniroma1.it>
2024-10-17 12:01:08 +0200

    Second HomeWork added

--- HomeWork_1/Form1.Designer.cs
+++ HomeWork_1/Form1.Designer.cs
@@ -39,13 +39,21 @@
             this.button1 = new System.Windows.Forms.Button();
             this.averageBox = new System.Windows.Forms.TextBox();
             this.average = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.time = new System.Windows.Forms.TextBox();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.variaceLabel = new System.Windows.Forms.Label();
+            this.varianceBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // hackerText
             // 
-            this.hackerText.Location = new System.Drawing.Point(16, 53);
+            this.hackerText.Location = new System.Drawing.Point(35, 51);
             this.hackerText.Name = "hackerText";
             this.hackerText.Size = new System.Drawing.Size(163, 20);
             this.hackerText.TabIndex = 0;
@@ -53,7 +61,7 @@
             // hacker
             // 
             this.hacker.AutoSize = true;
-            this.hacker.Location = new System.Drawing.Point(13, 37);
+            this.hacker.Location = new System.Drawing.Point(32, 35);
             this.hacker.Name = "hacker";
             this.hacker.Size = new System.Drawing.Size(83, 13);
             this.hacker.TabIndex = 1;
@@ -61,38 +69,38 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(16, 79);
+            this.pictureBox1.Location = new System.Drawing.Point(35, 104);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(470, 283);
+            this.pictureBox1.Size = new System.Drawing.Size(682, 281);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // serverText
             // 
-            this.serverText.Location = new System.Drawing.Point(185, 53);
+            this.serverText.Location = new System.Drawing.Point(204, 51);
             this.serverText.Name = "serverText";
             this.serverText.Size = new System.Drawing.Size(179, 20);
             this.serverText.TabIndex = 3;
             // 
             // probText
             // 
-            this.probText.Location = new System.Drawing.Point(370, 53);
+            this.probText.Location = new System.Drawing.Point(389, 51);
             this.probText.Name = "probText";
             this.probText.Size = new System.Drawing.Size(194, 20);
             this.probText.TabIndex = 4;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(492, 79);
+            this.pictureBox2.Location = new System.Drawing.Point(723, 104);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(483, 283);
+            this.pictureBox2.Size = new System.Drawing.Size(377, 281);
             this.pictureBox2.TabIndex = 5;
             this.pictureBox2.TabStop = false;
             // 
             // server
             // 
             this.server.AutoSize = true;
-            this.server.Location = new System.Drawing.Point(185, 37);
+            this.server.Location = new System.Drawing.Point(204, 35);
             this.server.Name = "server";
             this.server.Size = new System.Drawing.Size(79, 13);
             this.server.TabIndex = 6;
@@ -101,7 +109,7 @@
             // probability
             // 
             this.probability.AutoSize = true;
-            this.probability.Location = new System.Drawing.Point(370, 37);
+            this.probability.Location = new System.Drawing.Point(389, 35);
             this.probability.Name = "probability";
             this.probability.Size = new System.Drawing.Size(92, 13);
             this.probability.TabIndex = 7;
@@ -109,7 +117,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(571, 50);
+            this.button1.Location = new System.Drawing.Point(709, 48);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(171, 23);
             this.button1.TabIndex = 8;
@@ -119,26 +127,81 @@
             // 
             // averageBox
             // 
-            this.averageBox.Location = new System.Drawing.Point(492, 371);
+            this.averageBox.Location = new System.Drawing.Point(188, 714);
             this.averageBox.Name = "averageBox";
             this.averageBox.ReadOnly = true;
-            this.averageBox.Size = new System.Drawing.Size(110, 20);
+            this.averageBox.Size = new System.Drawing.Size(118, 20);
             this.averageBox.TabIndex = 9;
             // 
             // average
             // 
             this.average.AutoSize = true;
-            this.average.Location = new System.Drawing.Point(336, 374);
+            this.average.Location = new System.Drawing.Point(32, 717);
             this.average.Name = "average";
             this.average.Size = new System.Drawing.Size(150, 13);
             this.average.TabIndex = 10;
             this.average.Text = "Average of successful attacks";
             // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(35, 391);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(529, 305);
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(571, 391);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(529, 305);
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // time
+            // 
+            this.time.Location = new System.Drawing.Point(590, 50);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(100, 20);
+            this.time.TabIndex = 13;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(589, 35);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(61, 13);
+            this.timeLabel.TabIndex = 14;
+            this.timeLabel.Text = "Time stamp";
+            // 
+            // variaceLabel
+            // 
+            this.variaceLabel.AutoSize = true;
+            this.variaceLabel.Location = new System.Drawing.Point(342, 720);
+            this.variaceLabel.Name = "variaceLabel";
+            this.variaceLabel.Size = new System.Drawing.Size(49, 13);
+            this.variaceLabel.TabIndex = 15;
+            this.variaceLabel.Text = "Variance";
+            // 
+            // variance
+            // 
+            this.varianceBox.Location = new System.Drawing.Point(397, 714);
+            this.varianceBox.Name = "variance";
+            this.varianceBox.ReadOnly = true;
+            this.varianceBox.Size = new System.Drawing.Size(100, 20);
+            this.varianceBox.TabIndex = 16;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(992, 432);
+            this.ClientSize = new System.Drawing.Size(1364, 778);
+            this.Controls.Add(this.varianceBox);
+            this.Controls.Add(this.variaceLabel);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.average);
             this.Controls.Add(this.averageBox);
             this.Controls.Add(this.button1);
@@ -154,6 +217,8 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -172,6 +237,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox averageBox;
         private System.Windows.Forms.Label average;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.TextBox time;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label variaceLabel;
+        private System.Windows.Forms.TextBox varianceBox;
     }
 }
 
--- HomeWork_1/Form1.cs
+++ HomeWork_1/Form1.cs
@@ -29,10 +29,18 @@ namespace HomeWork_1
             histoGram.Clear(Color.White);
 
 
+            Graphics randomWalk = pictureBox3.CreateGraphics();
+            randomWalk.Clear(Color.White);
+
+            Graphics relativeWalk = pictureBox4.CreateGraphics();
+            relativeWalk.Clear(Color.White);
+
+
             int hacker = int.Parse(hackerText.Text);
             int server = int.Parse(serverText.Text);
-            float probablity = float.Parse(probText.Text);
+            float probability = float.Parse(probText.Text);
 
+            int timeStamp = int.Parse(time.Text);
             
             float lineWidth = pictureBox1.Width / (float)server;
             float lineHeight = pictureBox1.Height / (float)server;
@@ -41,36 +49,52 @@ namespace HomeWork_1
             PointF point1 = new PointF(0.0f, 0.0f);
 
 
-            List<int> scores = DrawGraph(graph, server, hacker, probablity, lineWidth, lineHeight);
+            List<int> scores = DrawGraph(graph, server, hacker, probability, lineWidth, lineHeight, timeStamp);
+
+            //score +1/-1
+            List<int> realistic = realisticWalk(randomWalk, server, hacker, probability, lineWidth, lineHeight);
+
+            List<int> relative = relativisticWalk(relativeWalk, server, hacker, probability, lineWidth, lineHeight);
 
 
             double averageScore = recursiveAverage(scores);
             averageBox.Text = averageScore.ToString("F2");
 
-            DrawHistogram(histoGram, hacker, server, scores, lineHeight);
+            DrawHistogram1(histoGram, hacker, server, scores, lineHeight);
+
+            double variance = CalculateVariance(realistic);
+            varianceBox.Text = variance.ToString("F2");
         }
 
-        private List<int> DrawGraph(Graphics graph, int servers, int hackers, float prob, float width, float height)
+        private List<int> DrawGraph(Graphics graph, int servers, int hackers, float prob, float width, float height, int timeStamp)
         {
             List<int> totalscores = new List<int>();
+            List<int> partialScores = new List<int>();
+
 
             float lineWidth = pictureBox1.Width / (float)servers;
             float lineHeight = pictureBox1.Height / (float)servers;
 
             Random rnd = new Random();
 
+
             for (int i = 0; i < hackers; i++)
             {
+
                 Pen p = new Pen(Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255)));
 
                 int score = 0;
 
+
                 PointF start = new PointF(0.0f, pictureBox1.Height);
+
 
                 for (int j = 0; j < servers; j++)
                 {
+
                     if (rnd.NextDouble() <= prob)
                     {
+
                         PointF end = new PointF(start.X, start.Y - lineHeight);
                         graph.DrawLine(p, start, end);
                         start = end;
@@ -78,19 +102,77 @@ namespace HomeWork_1
                     }
 
                     PointF next = new PointF(start.X + lineWidth, start.Y);
-
                     graph.DrawLine(p, start, next);
-
                     start = next;
 
-                }
-                totalscores.Add(score); //list of the hacker's score
-            }
 
-            return totalscores;
+                    if (j == timeStamp - 1)
+                    {
+                        partialScores.Add(score);
+                    }
+                }
+
+                totalscores.Add(score);
+            }
+            float xOffset = lineWidth * timeStamp;   
+            float yOffset = pictureBox1.Height;      
+
+            DrawHistogram2(graph, hackers, servers, partialScores, height, xOffset, yOffset);
+
+            return totalscores; 
         }
 
-        private void DrawHistogram(Graphics graph, int hackers, int servers, List<int> scores, float height)
+        private List<int> realisticWalk(Graphics randomWalk, int hackers, int servers, float prob, float width, float height)
+        {
+            List<int> totalScores = new List<int>();
+
+            float lineWidth = pictureBox3.Width / (float)servers;
+            float lineHeight = pictureBox3.Height / (float)servers;
+
+            Random rnd = new Random();
+            for (int i = 0; i < hackers; i++)
+            {
+                Pen p = new Pen(Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255)));
+
+                int score = 0;
+
+                PointF start = new PointF(0.0f, pictureBox3.Height / 2);
+
+                for (int j = 0; j < servers; j++)
+                {
+                    // Check if the hacker penetrates the server
+                    if (rnd.NextDouble() <= prob)
+                    {
+                        // Move the line upwards (success) and increase the score
+                        PointF end = new PointF(start.X, start.Y - lineHeight); // Go up one step
+                        randomWalk.DrawLine(p, start, end);
+                        start = end;
+                        score++;
+                    }
+
+                    else
+                    {
+                        // Move the line downwards (failure) and decrease the score
+                        PointF end = new PointF(start.X, start.Y + lineHeight); // Go down one step
+                        randomWalk.DrawLine(p, start, end);
+                        start = end;
+                        score--;  // Decrease the score
+                    }
+
+                    // Move horizontally to the next server
+                    PointF next = new PointF(start.X + lineWidth, start.Y);
+                    randomWalk.DrawLine(p, start, next);
+                    start = next;
+                }
+                totalScores.Add(score); //list of the hacker's score
+            }
+
+            return totalScores;
+
+        }
+
+
+        private void DrawHistogram1(Graphics graph, int hackers, int servers, List<int> scores, float height)
         {
             Pen p = new Pen(Color.White);
 
@@ -122,6 +204,99 @@ namespace HomeWork_1
             }
         }
 
+        private void DrawHistogram2(Graphics graph, int hackers, int servers, List<int> scores, float height, float xOffset, float yOffset)
+        {
+            Pen p = new Pen(Color.White);
+            Pen penna = new Pen(Color.Black);
+
+            var g = scores.GroupBy(i => i).OrderBy(group => group.Key);
+
+            var m1 = scores.GroupBy(i => i).OrderByDescending(group => group.Count()).First().Count();
+
+            
+            int diffScores = scores.Distinct().Count();
+
+           
+            float scoreHeigth = (pictureBox2.Width * 0.9f) / (float)m1 * 0.3f; 
+            float scoreWidth = (pictureBox2.Height * 0.6f) / (float)diffScores * 0.3f;
+
+            
+            foreach (var grp in g)
+            {
+                SolidBrush brush = new SolidBrush(Color.Aquamarine);
+
+                
+                float rectX = xOffset;  
+                float rectY = yOffset - (height * grp.Key); 
+
+                RectangleF rect = new RectangleF(rectX, rectY, scoreHeigth * grp.Count(), height);
+
+                graph.FillRectangle(brush, rect);
+                graph.DrawRectangle(p, rectX, rectY, scoreHeigth * grp.Count(), height);
+            }
+        }
+
+        private List<int> relativisticWalk(Graphics relativeWalk, int servers, int hackers, float probability, float lineWidth, float lineHeight)
+        {
+            Random rnd = new Random();
+            List<int> totalScores = new List<int>();  // Lista per contenere gli score di tutti gli hacker
+
+            // Itera su ogni hacker
+            for (int i = 0; i < hackers; i++)
+            {
+                // Inizializza score per l'hacker corrente
+                int jump = 0;
+                int attempts = 0;
+
+                // Punto di partenza in basso a sinistra
+                PointF start = new PointF(0.0f, pictureBox4.Height / 2);
+
+                
+                Pen p = new Pen(Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255)));
+
+                for (int j = 0; j < servers; j++)
+                {
+                    attempts++;  
+
+                    
+                    bool penetrationSuccess = rnd.NextDouble() <= probability;
+
+                    
+                    if (penetrationSuccess)
+                    {
+                        jump++;  
+                    }
+
+                    float stepSize = lineHeight * (float)jump / attempts; // Movimento proporzionale
+
+                    // Determina la direzione del passo
+                    PointF end;
+                    if (penetrationSuccess)
+                    {
+                        end = new PointF(start.X, start.Y - stepSize); // Salita
+                    }
+                    else
+                    {
+                        end = new PointF(start.X, start.Y + stepSize); // Discesa
+                    }
+
+                    relativeWalk.DrawLine(p, start, end);
+                    start = end;
+                   
+                    PointF next = new PointF(start.X + lineWidth, start.Y);
+                    relativeWalk.DrawLine(p, start, next);
+                    start = next;
+                }
+
+                
+                totalScores.Add(jump);
+            }
+
+            return totalScores;
+        }
+
+
+
         private double recursiveAverage(List<int> scores)
         {    
             double recursiveSum(List<int> list)
@@ -140,6 +315,32 @@ namespace HomeWork_1
                 return 0;
             }
             return recursiveSum(scores) / scores.Count; 
+        }
+
+        public static double CalculateVariance(List<int> data)
+        {
+            if (data.Count == 0)
+            {
+                throw new ArgumentException("The data list cannot be empty.");
+            }
+
+            // Calculate the mean
+            double mean = 0;
+            foreach (var number in data)
+            {
+                mean += number;
+            }
+            mean /= data.Count;
+
+            // Calculate the variance
+            double variance = 0;
+            foreach (var number in data)
+            {
+                variance += Math.Pow(number - mean, 2);
+            }
+            variance /= (data.Count - 1); // Sample variance
+
+            return variance;
         }
 
     }
